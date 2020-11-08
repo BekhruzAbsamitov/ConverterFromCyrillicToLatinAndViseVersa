e97061e06b2a5b08f57e9f90083f95a41ce20b05
BekhruzAbsamitov <absamitovbekhruz@gmail.com>
2020-11-09 00:38:16 +0500

    commit#CompletedProject

--- src/ConvertFromCyrillicToLatinAndViceVersa.java
+++ src/ConvertFromCyrillicToLatinAndViceVersa.java
@@ -3,21 +3,31 @@ import java.util.Scanner;
 
 public class ConvertFromCyrillicToLatinAndViceVersa {
 
-    static String[] kirill = {"Ў", "ў", "Ғ", "ғ", "Ё", "Ц", "Ч", "Ш", "Я", "Ғ", "ғ", "Ў", "ў", "Я", "я", "Ю", "ю", "Ш", "ш", "Ч", "ч", "Ц", "ц", "Ё", "ё", "А", "а", "Б", "б", "В", "в", "Г", "г",
-            "Д", "д", "Е", "е", "Ж", "ж", "З", "з",
-            "И", "и", "Й", "й", "К", "к", "Л", "л", "М", "м",
-            "Н", "н", "О", "о", "П", "п", "Р", "р", "С", "с",
-            "Т", "т", "У", "у", "Ф", "ф", "Х", "х",
-            "Э", "э",
-            "Қ", "қ", "Ҳ", "ҳ", ".", ",", " ", "?", "!"};
+    static String[] cyrillicAlphabet = {
+            "Ў", "ў", "Ғ", "ғ", "Ё", "Ц", "Ч", "Ш",
+            "Я", "Ғ", "ғ", "Ў", "ў", "Я", "я", "Ю",
+            "ю", "Ш", "ш", "Ч", "ч", "Ц", "ц", "Ё",
+            "ё", "А", "а", "Б", "б", "В", "в", "Г",
+            "г", "Д", "д", "Е", "е", "Ж", "ж", "З",
+            "з", "И", "и", "Й", "й", "К", "к", "Л",
+            "л", "М", "м", "Н", "н", "О", "о", "П",
+            "п", "Р", "р", "С", "с", "Т", "т", "У",
+            "у", "Ф", "ф", "Х", "х", "Э", "э", "Қ",
+            "қ", "Ҳ", "ҳ", ".", ",", " ", "?", "!"
+    };
 
-    static String[] latin = {"O‘", "o‘", "G‘", "g‘", "Yo", "Ts", "Ch", "Sh", "Ya", "G'", "g'", "O'", "o'", "YA", "ya", "Yu", "yu", "SH", "sh", "CH", "ch", "TS", "ts", "YO", "yo", "А", "a", "B", "b", "V", "v", "G", "g",
-            "D", "d", "E", "e", "J", "j", "Z", "z",
-            "I", "i", "Y", "y", "K", "k", "L", "l", "M", "m",
-            "N", "n", "O", "o", "P", "p", "R", "r", "S", "s",
-            "T", "t", "U", "u", "F", "f", "H", "h",
-            "E", "e",
-            "Q", "q", "X", "x", ".", ",", " ", "?", "!"};
+    static String[] latinAlphabet = {
+            "O‘", "o‘", "G‘", "g‘", "Yo", "Ts", "Ch",
+            "Sh", "Ya", "G'", "g'", "O'", "o'", "YA",
+            "ya", "Yu", "yu", "SH", "sh", "CH", "ch",
+            "TS", "ts", "YO", "yo", "А", "a", "B", "b",
+            "V", "v", "G", "g", "D", "d", "E", "e", "J",
+            "j", "Z", "z", "I", "i", "Y", "y", "K", "k",
+            "L", "l", "M", "m", "N", "n", "O", "o", "P",
+            "p", "R", "r", "S", "s", "T", "t", "U", "u",
+            "F", "f", "H", "h", "E", "e", "Q", "q", "X",
+            "x", ".", ",", " ", "?", "!"
+    };
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
@@ -25,14 +35,14 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
         File file = new File("src/main/java/extra_Task/Kiril.txt");
         File file1 = new File("src/main/java/extra_Task/Latin.txt");
 
-        System.out.println("Choose language:\n1. Cyrilic to Latin\n2. Latin to Cyrilic");
+        System.out.println("1. Cyrillic to Latin\n2. Latin to Cyrillic");
         int n = scanner.nextInt();
         if (n == 1) {
             try (BufferedReader reader = new BufferedReader(new FileReader(file));
                  BufferedWriter writer = new BufferedWriter(new FileWriter(file1))) {
                 String c;
                 while ((c = reader.readLine()) != null) {
-                    String s = convertCyrillic(c);
+                    String s = convertFromCyrillicToLatin(c);
                     writer.write(s + "\n");
                 }
             }
@@ -42,7 +52,7 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
                  BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
                 String c;
                 while ((c = reader.readLine()) != null) {
-                    String s = convertLatin(c);
+                    String s = convertFromLatinToCyrillic(c);
                     writer.write(s + "\n");
                 }
             }
@@ -50,11 +60,11 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
 
     }
 
-    public static String convertCyrillic(String message) {
+    public static String convertFromCyrillicToLatin(String message) {
         scanner = new Scanner(System.in);
         int counter = 0;
         for (int i = 0; i < message.length() - 1; i++) {
-            for (String s : latin) {
+            for (String s : latinAlphabet) {
                 if (!(i == message.length() - 2 || i == message.length() - 1)) {
                     if (s.equals(String.valueOf(message.charAt(i)).concat(String.valueOf(message.charAt(i + 1))))) {
                         i++;
@@ -70,7 +80,7 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
         for (int i = 0; i < message.length(); i++) {
 
             int idKir = 0;
-            for (String s : kirill) {
+            for (String s : cyrillicAlphabet) {
                 if (i == message.length() - 1) {
                     if (s.equals(String.valueOf(message.charAt(i)))) {
                         temp[tempCounter] = idKir;
@@ -80,7 +90,6 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
                     idKir++;
                 } else {
                     if (s.equals(String.valueOf(message.charAt(i)).concat(String.valueOf(message.charAt(i + 1))))) {
-
                         temp[tempCounter] = idKir;
                         tempCounter++;
                         i++;
@@ -88,7 +97,6 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
                     } else if (s.equals(String.valueOf(message.charAt(i)))) {
                         temp[tempCounter] = idKir;
                         tempCounter++;
-
                         break;
                     }
                     idKir++;
@@ -97,19 +105,19 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
         }
         StringBuilder retValue = new StringBuilder();
         for (int i = 0; i < temp.length + counter; i++) {
-            for (int j = 0; j < kirill.length; j++) {
+            for (int j = 0; j < cyrillicAlphabet.length; j++) {
                 if (temp[i] == j) {
-                    retValue.append(latin[j]);
+                    retValue.append(latinAlphabet[j]);
                 }
             }
         }
         return retValue.toString();
     }
 
-    public static String convertLatin(String message) {
+    public static String convertFromLatinToCyrillic(String message) {
         int counter = 0;
         for (int i = 0; i < message.length() - 1; i++) {
-            for (String s : latin) {
+            for (String s : latinAlphabet) {
                 if (!(i == message.length() - 2 || i == message.length() - 1)) {
                     if (s.equals(String.valueOf(message.charAt(i)).concat(String.valueOf(message.charAt(i + 1))))) {
                         i++;
@@ -126,8 +134,7 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
         for (int i = 0; i < message.length(); i++) {
 
             int idLat = 0;
-            int size = latin.length;
-            for (String s : latin) {
+            for (String s : latinAlphabet) {
                 if (i == message.length() - 1) {
                     if (s.equals(String.valueOf(message.charAt(i)))) {
                         temp[tempCounter] = idLat;
@@ -154,9 +161,9 @@ public class ConvertFromCyrillicToLatinAndViceVersa {
 
         StringBuilder retValue = new StringBuilder();
         for (int i = 0; i < temp.length - counter; i++) {
-            for (int j = 0; j < kirill.length; j++) {
+            for (int j = 0; j < cyrillicAlphabet.length; j++) {
                 if (temp[i] == j) {
-                    retValue.append(kirill[j]);
+                    retValue.append(cyrillicAlphabet[j]);
                 }
             }
         }
